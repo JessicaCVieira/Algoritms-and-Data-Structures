a575e41d278e4cdb170bee4c9e67645845211abd
JessicaCVieira <119906775+JessicaCVieira@users.noreply.github.com>
2023-02-08 23:12:04 +0000

    Initial commit

---
+++ src/Semana01/Date.java
@@ -0,0 +1,32 @@
+package Semana01;
+
+
+class Date {
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public Date(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+    public int getMes() {
+        return mes;
+    }
+    public int getAno() {
+        return ano;
+    }
+
+    
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+}
