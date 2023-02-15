d6c733c6be1462d0d2216b0dd71c70c1d7d64475
JessicaCVieira <119906775+JessicaCVieira@users.noreply.github.com>
2023-02-15 20:58:11 +0000

    QuickFindUF

---
+++ src/Semana02/QuickFindUF.java
@@ -0,0 +1,42 @@
+package Semana02;
+
+public class QuickFindUF{
+
+    private int[] id; 
+
+    public QuickFindUF(int N){
+        id = new int[N];
+        for (int i = 0; i < N; i++){
+            id[i] = i;
+        }
+    }
+
+    public boolean connected(int p){
+        return id[p] == id[q]; //ñ sei pq é que está a dar erro, está igual aos dois ptt
+    }
+    
+    public void union(int p, int q){
+        int pid = id[p];
+        int qid = id[q];
+        for (int i = 0; i < id.length; i++){
+            if (id[i] == pid){
+                id[i] = qid;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        for(int i = 1000; i < 256000 ; i*= 2){
+            QuickFindUF unionFind = new QuickFindUF(i);
+            long start = System.currentTimeMillis(); 
+            for(int j = 0; j < i; j++){
+                //TODO
+                unionFind.union(random, random);
+            }
+            long end = System.currentTimeMillis(); 
+        }
+
+    }
+
+}
+
